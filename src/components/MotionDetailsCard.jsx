bef9892411f639ac138933ec93f8b3ed6f0747c2
Grace Shamer <gs1018@yahoo.com>
2025-12-16 13:55:23 -0500

    change detailscard

--- src/components/MotionDetailsCard.jsx
+++ src/components/MotionDetailsCard.jsx
@@ -44,32 +44,7 @@ function MotionDetailsCard({ motion = {}, onClick }) {
     }, [motion, getUsernameById]);
 
     const statusLabel = motion.status || (motion.second ? 'SECONDED' : 'PENDING');
-    const getChipSxForStatus = (label) => {
-        const s = String(label || '').toUpperCase();
-        // choose text color for readability on the given background
-        const lightText = { color: 'white' };
-        const darkText = { color: 'black' };
-
-        switch (s) {
-            case 'DEBATE':
-                return { sx: { bgcolor: '#FF57BB', ...lightText } };
-            case 'VOTING':
-                return { sx: { bgcolor: '#22577A', ...lightText } };
-            case 'PASSED':
-                return { sx: { bgcolor: '#57CC99', ...darkText } };
-            case 'SECONDED':
-                return { sx: { bgcolor: '#38A3A5', ...darkText } };
-            case 'VETOED':
-                return { sx: { bgcolor: '#4f4e4eff', ...lightText } };
-            case 'REJECTED':
-                return { sx: { bgcolor: '#8d0858ff', ...lightText } };
-            case 'PROPOSED':
-                return {sx: {bgcolor: '#85D4D5', ...darkText}};
-            case 'PENDING':
-            default:
-                return { sx: {} };
-        }
-    };
+    
 
     return (
         <Card
